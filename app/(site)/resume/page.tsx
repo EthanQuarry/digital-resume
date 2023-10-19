3b1db3676fb17c70316c423b053d4959ba9debd0
mikam-dev <kamilosmike@gmail.com>
2023-10-19 15:26:24 -0500

    adding contact form

--- app/(site)/resume/page.tsx
+++ app/(site)/resume/page.tsx
@@ -1,12 +1,15 @@
+import { Experiences } from "@/components/content/Experiences";
 import { siteConfig } from "@/config";
 
-export default function ResumePage() {
+export default async function ResumePage() {
+
 	return (
 		<>
 			<title>Resume | {siteConfig.title}</title>
 
 			<section className="py-16">
 				<div className="container">
+					<Experiences />
 				</div>
 			</section>
 		</>
---
+++ components/content/Experiences.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { getExperiences } from "@/sanity/queries";
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+import { PortableText } from '@portabletext/react';
+
+export async function Experiences() {
+	const experiences = await getExperiences();
+
+	return (
+		<div className="w-full px-4 flex flex-col items-center justify-start">
+			<div className="w-full max-w-4xl flex flex-col items-center justify-evenly">
+				{experiences.map((experience: any, index: number) => (
+					<Card className="w-full mb-6 p-4" key={index}>
+						<CardHeader>
+							<h1 className='text-3xl'>{experience.company}</h1>
+							<h2 className='text-2xl'>{experience.title}</h2>
+						</CardHeader>
+						<CardContent>
+							<div className="mb-2 flex w-full max-w-lg">
+								<h3 className='text-md'><em>{experience.start}</em></h3>
+								&nbsp;<p>to</p>&nbsp;
+								<h3 className='text-md'><em>{experience.end}</em></h3>
+							</div>
+							<PortableText value={experience.description} />
+						</CardContent>
+					</Card>
+				))}
+			</div>
+		</div>
+	)
+}
