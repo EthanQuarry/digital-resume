c7dbec54849a0d00247f60384ca6f900a91a09a9
Mike Kamilos <64214237+mikam-dev@users.noreply.github.com>
2023-10-22 13:51:20 -0500

    Update page.tsx

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -9,7 +9,7 @@ export default async function ContactPage() {
 			<title>Contact | {siteConfig.title}</title>
 			<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
-			<section className="py-16 w-full min-h-[calc(100vh-12rem)]">
+			<section className="py-8 w-full min-h-[calc(100vh-12rem)]">
 				<div className="container w-full flex flex-col justify-around items-center">
 					<h1 className="mb-6 text-2xl sm:text-4xl md:text-5xl">Get in touch with me.</h1>
 					<div className="mt-6 p-4 flex justify-center items-center w-full">
