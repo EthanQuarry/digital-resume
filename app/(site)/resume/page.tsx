e7538b5a4315b81625d75ed507cf773df44ba483
Mike Kamilos <64214237+mikam-dev@users.noreply.github.com>
2023-10-29 13:00:34 -0500

    Update page.tsx

--- app/(site)/resume/page.tsx
+++ app/(site)/resume/page.tsx
@@ -1,4 +1,5 @@
 import { Experiences } from "@/components/content/Experiences";
+import { Intro } from "@/components/content/Intro";
 import { siteConfig } from "@/config";
 
 export default async function ResumePage() {
@@ -6,7 +7,10 @@ export default async function ResumePage() {
 	return (
 		<>
 			<title>Resume | {siteConfig.title}</title>
-
+            <section className="mb-2">
+                <div className="container">
+					<Intro />
+				</div>
 			<section className="py-4">
 				<div className="container">
 					<Experiences />
